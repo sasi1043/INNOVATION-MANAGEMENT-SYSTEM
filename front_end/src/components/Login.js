15d8e3afeff5197e206ccb9bfc283c8025202c5c
SASIKUMAR <149701946+sasi1043@users.noreply.github.com>
2026-02-28 11:06:13 +0530

    Update Login.js

--- front_end/src/components/Login.js
+++ front_end/src/components/Login.js
@@ -26,24 +26,48 @@ function Login() {
 
   return (
     <div className="d-flex justify-content-center align-items-center home-bg vh-100">
-      <div className="text-center">
-        {error && <p className="text-danger fw-bold">{error}</p>}
-
-        <h4>INNOVATION MANAGEMENT SYSTEM</h4>
-        <h5>Welcome</h5>
-
-        <img src={logo} height={150} width={150} alt="ims-logo" />
-
-        <div className="mt-4">
-          <Button
-            variant="contained"
+        <div className=" text-center">
+          {error && (
+          <p className="text-danger mt-3 fw-bold">
+            {error}
+          </p>
+        )}
+          <div >
+          <h4>INNOVATION MANAGEMENT SYSTEM</h4>
+          <h5>Welcome</h5>
+          <div>
+            <img src={logo} height={150} width={150}  alt='ims-logo' style={{borderRadius:"50px"}}></img>
+          </div>
+          <div className='d-flex justify-content-center mt-4 w-100'>
+        <Button
+               variant="contained"
+               startIcon={
+               <img
+                src="https://developers.google.com/identity/images/g-logo.png"
+                 alt="Google"
+                 style={{ width: 18, height: 18 ,borderRadius:"50px" }}
+                />
+                }
+                 sx={{
+                   backgroundColor: "#1a73e8", // Google dark blue
+                   color: "white",
+                   textTransform: "none",
+                   fontWeight: 500,
+                   borderRadius: "8px",
+                   px: 2,
+                   "&:hover": {
+                   backgroundColor: "#1558b0",
+                },
+              }}
+            className="w-100"
             onClick={handleLogin}
-            sx={{ backgroundColor: "#1a73e8" }}
           >
-            Sign in with Google
-          </Button>
+       Sign in with Google
+      </Button>
+          </div>
+          </div>
         </div>
-      </div>
+        
     </div>
   );
 }
